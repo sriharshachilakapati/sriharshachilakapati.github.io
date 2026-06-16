7e7ec43e264190dc470b95529cd136dee7368ec1
Sri Harsha Chilakapati <hello@goharsha.com>
2026-06-16 21:54:02 +0530

    Updated MathJax to v3 and JQuery, along with JS loading optimizations

--- assets/revealjs/plugin/math/math.js
+++ assets/revealjs/plugin/math/math.js
@@ -4,64 +4,57 @@
  *
  * @author Hakim El Hattab
  */
-var RevealMath = window.RevealMath || (function(){
+var RevealMath =
+  window.RevealMath ||
+  (function() {
+    var options = Reveal.getConfig().math || {};
+    // Default to MathJax v3 loader (async)
+    options.mathjax =
+      options.mathjax ||
+      "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
 
-	var options = Reveal.getConfig().math || {};
-	options.mathjax = options.mathjax || 'https://cdn.mathjax.org/mathjax/latest/MathJax.js';
-	options.config = options.config || 'TeX-AMS_HTML-full';
+    loadScript(options.mathjax, function() {
+      // MathJax v3 uses a different API. Configure if present.
+      if (window.MathJax && MathJax.startup) {
+        // Ensure MathJax processes math in slides when shown
+        MathJax.startup.promise.then(function() {
+          // Initial typeset
+          MathJax.typesetPromise();
+          Reveal.layout();
+        });
 
-	loadScript( options.mathjax + '?config=' + options.config, function() {
+        Reveal.addEventListener("slidechanged", function(event) {
+          MathJax.typesetPromise([event.currentSlide]).catch(function(err) {
+            console.warn("MathJax typeset failed:", err);
+          });
+        });
+      }
+    });
 
-		MathJax.Hub.Config({
-			messageStyle: 'none',
-			tex2jax: {
-				inlineMath: [['$','$'],['\\(','\\)']] ,
-				skipTags: ['script','noscript','style','textarea','pre']
-			},
-			skipStartupTypeset: true
-		});
+    function loadScript(url, callback) {
+      var head = document.querySelector("head");
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = url;
 
-		// Typeset followed by an immediate reveal.js layout since
-		// the typesetting process could affect slide height
-		MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub ] );
-		MathJax.Hub.Queue( Reveal.layout );
+      // Wrapper for callback to make sure it only fires once
+      var finish = function() {
+        if (typeof callback === "function") {
+          callback.call();
+          callback = null;
+        }
+      };
 
-		// Reprocess equations in slides when they turn visible
-		Reveal.addEventListener( 'slidechanged', function( event ) {
+      script.onload = finish;
 
-			MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub, event.currentSlide ] );
+      // IE
+      script.onreadystatechange = function() {
+        if (this.readyState === "loaded") {
+          finish();
+        }
+      };
 
-		} );
-
-	} );
-
-	function loadScript( url, callback ) {
-
-		var head = document.querySelector( 'head' );
-		var script = document.createElement( 'script' );
-		script.type = 'text/javascript';
-		script.src = url;
-
-		// Wrapper for callback to make sure it only fires once
-		var finish = function() {
-			if( typeof callback === 'function' ) {
-				callback.call();
-				callback = null;
-			}
-		}
-
-		script.onload = finish;
-
-		// IE
-		script.onreadystatechange = function() {
-			if ( this.readyState === 'loaded' ) {
-				finish();
-			}
-		}
-
-		// Normal browsers
-		head.appendChild( script );
-
-	}
-
-})();
+      // Normal browsers
+      head.appendChild(script);
+    }
+  })();
