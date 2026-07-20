2343e402d5b6415094fbe3d868932fcd12d9ac52
Sri Harsha Chilakapati <hello@goharsha.com>
2026-07-20 12:27:48 +0530

    Remove non-present directory in minify-images.js

--- minify-images.js
+++ minify-images.js
@@ -1,115 +1,113 @@
-import imagemin from 'imagemin';
-import imageminJpegtran from 'imagemin-jpegtran';
-import imageminPngquant from 'imagemin-pngquant';
-import imageminGifsicle from 'imagemin-gifsicle';
-import imageminSvgo from 'imagemin-svgo';
+import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
+import imageminPngquant from "imagemin-pngquant";
+import imageminGifsicle from "imagemin-gifsicle";
+import imageminSvgo from "imagemin-svgo";
 
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
-const allowedExtensions = [
-    "jpg",
-    "jpeg",
-    "gif",
-    "png",
-    "svg"
-]
+const allowedExtensions = ["jpg", "jpeg", "gif", "png", "svg"];
 
 async function minifyImages(directory) {
-    await imagemin([ `${directory}/*.{${ allowedExtensions.join(",") }}` ], {
-        destination: directory,
+  await imagemin([`${directory}/*.{${allowedExtensions.join(",")}}`], {
+    destination: directory,
+    strip: true,
+    verbose: true,
+    plugins: [
+      imageminJpegtran(),
+      imageminGifsicle(),
+      imageminPngquant({
         strip: true,
-        verbose: true,
+        quality: [0.8, 1.0],
+      }),
+      imageminSvgo({
         plugins: [
-            imageminJpegtran(),
-            imageminGifsicle(),
-            imageminPngquant({
-                strip: true,
-                quality: [ 0.8, 1.0 ]
-            }),
-            imageminSvgo({
-                plugins: [{
-                    name: 'removeViewBox',
-                    active: false
-                }]
-            })
-        ]
-    });
+          {
+            name: "removeViewBox",
+            active: false,
+          },
+        ],
+      }),
+    ],
+  });
 }
 
 async function findAssetDirectories(currentPath) {
-    const paths = [];
-    const files = await fs.readdir(currentPath);
+  const paths = [];
+  const files = await fs.readdir(currentPath);
 
-    for (const file of files) {
-        const path = `${currentPath}/${file}`;
-        const stat = await fs.stat(path);
+  for (const file of files) {
+    const path = `${currentPath}/${file}`;
+    const stat = await fs.stat(path);
 
-        if (!stat.isDirectory()) {
-            continue;
-        }
-
-        paths.push(path);
-        paths.push(... await findAssetDirectories(path));
+    if (!stat.isDirectory()) {
+      continue;
     }
 
-    return paths;
+    paths.push(path);
+    paths.push(...(await findAssetDirectories(path)));
+  }
+
+  return paths;
 }
 
 async function getFileSizes(sizes, directory) {
-    const files = await fs.readdir(directory);
+  const files = await fs.readdir(directory);
 
-    for (const file of files) {
-        const path = `${directory}/${file}`;
-        const stat = await fs.stat(path);
+  for (const file of files) {
+    const path = `${directory}/${file}`;
+    const stat = await fs.stat(path);
 
-        if (stat.isDirectory() || allowedExtensions.filter(it => path.endsWith(`.${it}`)).length == 0) {
-            continue;
-        }
-
-        sizes[path] = stat.size;
+    if (
+      stat.isDirectory() ||
+      allowedExtensions.filter((it) => path.endsWith(`.${it}`)).length == 0
+    ) {
+      continue;
     }
+
+    sizes[path] = stat.size;
+  }
 }
 
 async function main() {
-    const directories = [];
+  const directories = [];
 
-    directories.push(
-        ... await findAssetDirectories('assets/images'),
-        ... await findAssetDirectories('assets/social-icons'),
-        ... await findAssetDirectories('ppt')
-    );
+  directories.push(
+    ...(await findAssetDirectories("assets/images")),
+    ...(await findAssetDirectories("ppt")),
+  );
 
-    const preSizes = {};
-    const postSizes = {};
+  const preSizes = {};
+  const postSizes = {};
 
-    for (const directory of directories) {
-        console.log(`Minifying files in directory ${directory}`);
+  for (const directory of directories) {
+    console.log(`Minifying files in directory ${directory}`);
 
-        await getFileSizes(preSizes, directory);
-        await minifyImages(directory);
-        await getFileSizes(postSizes, directory);
-    }
+    await getFileSizes(preSizes, directory);
+    await minifyImages(directory);
+    await getFileSizes(postSizes, directory);
+  }
 
-    const results = [];
+  const results = [];
 
-    for (const fileName in preSizes) {
-        const sizeBefore = preSizes[fileName];
-        const sizeAfter = postSizes[fileName];
-        const difference = ((sizeAfter - sizeBefore) / (sizeBefore)) * 100;
+  for (const fileName in preSizes) {
+    const sizeBefore = preSizes[fileName];
+    const sizeAfter = postSizes[fileName];
+    const difference = ((sizeAfter - sizeBefore) / sizeBefore) * 100;
 
-        results.push({
-            fileName,
-            sizeBefore,
-            sizeAfter,
-            difference: difference.toFixed(2)
-        });
-    }
+    results.push({
+      fileName,
+      sizeBefore,
+      sizeAfter,
+      difference: difference.toFixed(2),
+    });
+  }
 
-    if (results.length > 0) {
-        console.table(results);
-    } else {
-        console.log('All images are already optimized :)');
-    }
+  if (results.length > 0) {
+    console.table(results);
+  } else {
+    console.log("All images are already optimized :)");
+  }
 }
 
 await main();
